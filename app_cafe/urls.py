ee92c29cf42c46ae049d184cd80abbaef5f558bf
Xan <xannurov@mail.ru>
2024-04-13 23:26:02 +0300

    Add hot-dogs

--- app_cafe/urls.py
+++ app_cafe/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from app_cafe.views import index_page
+from app_cafe.views import index_page, hotdog_details_views
 
 urlpatterns = [
     path("", index_page, name='index_page'),
+    path("hotdog/<int:id>/", hotdog_details_views)
 ]
 
 if settings.DEBUG:
--- app_cafe/views.py
+++ app_cafe/views.py
@@ -7,3 +7,6 @@ def index_page(request):
     hotdogs = Hotdog.objects.all()
     return render(request, 'index.html', {'hotdogs': hotdogs})
 # Create your  views here.
+def hotdog_details_views(request, id):
+    hotdog = Hotdog.objects.get(id=id)
+    return render(request, 'detail.html', {'hotdog': hotdog})
