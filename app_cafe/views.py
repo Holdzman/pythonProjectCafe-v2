7f8b6b5d8f0bc41d88e3f938ae331457380daab2
Xan <xannurov@mail.ru>
2024-04-21 23:20:37 +0300

    deleted spaces
    fix Drinks
    fix youtube shorts

--- app_cafe/views.py
+++ app_cafe/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 
-from app_cafe.models import Hotdog
+from app_cafe.models import Hotdog, Drink
 
 
 def index_page(request):
     hotdogs = Hotdog.objects.all()
     return render(request, 'index.html', {'hotdogs': hotdogs})
+
+
 # Create your  views here.
 def hotdog_details_views(request, id):
     hotdog = Hotdog.objects.get(id=id)
     return render(request, 'detail.html', {'hotdog': hotdog})
+
+
+def index_page_two(request):
+    drinks = Drink.objects.all()
+    return render(request, 'index.html', {'drinks': drinks})
+
+
+def drink_details_views(request, id):
+    drink = Drink.objects.get(id=id)
+    return render(request, 'detail.html', {'drink': drink})
